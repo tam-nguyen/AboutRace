6c6b863d78933fe46027d44c500650045dc0a691
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-05-08 21:36:17 +0200

    add missing file

---
+++ src/components/overlay.js
@@ -0,0 +1,33 @@
+const React = require('react')
+import ReactDOM from 'react-dom'
+import styled, { css } from 'styled-components'
+
+class Portal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.el = document.createElement('div')
+  }
+
+  componentDidMount() {
+    // The portal element is inserted in the DOM tree after
+    // the Modal's children are mounted, meaning that children
+    // will be mounted on a detached DOM node. If a child
+    // component requires to be attached to the DOM tree
+    // immediately when mounted, for example to measure a
+    // DOM node, or uses 'autoFocus' in a descendant, add
+    // state to Modal and only render the children when Modal
+    // is inserted in the DOM tree.
+    document.body.appendChild(this.el)
+  }
+
+  componentWillUnmount() {
+    document.body.removeChild(this.el)
+  }
+
+  render() {
+      console.log('rendering portal???')
+    return ReactDOM.createPortal(this.props.children, this.el)
+  }
+}
+
+export default Portal
