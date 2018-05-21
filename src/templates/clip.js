289e9ceefbd9b01e2c7a5c09f02c9afb093dc9b3
Sam Bhagwat <calcsam@gmail.com>
2018-05-21 23:57:36 +0200

    working related content

--- src/templates/clip.js
+++ src/templates/clip.js
@@ -2,6 +2,13 @@ const React = require('react')
 import AllClips, { Clip } from '../components/allClips.js'
 import styled from 'styled-components'
 import Link from 'gatsby-link'
+import {
+  getCards,
+  QuickFactCard,
+  FAQCard,
+  ClipCard,
+  ArticleCard
+} from '../components/subtheme'
 
 const ChosenClipCard = styled.div`
   height: 414px;
@@ -32,22 +39,71 @@ const Chosen = styled.div`
 export const ChosenClip = ({ clip, link }) =>  {
 	return (
 		<Chosen>
-			<ChosenClipCard>
-			<iframe width='720px' height='100%' src={`${clip.field_external_video_url && clip.field_external_video_url.uri}?title=0&byline=0&portrait=0`} frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>			 
+		  <ChosenClipCard>
+			 <iframe width='720px' height='100%' src={`${clip.field_external_video_url && clip.field_external_video_url.uri}?title=0&byline=0&portrait=0`} frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 			</ChosenClipCard>
      <ChosenClipCaption> {clip.title}</ChosenClipCaption>
-  
 		</Chosen>
-		)
+	)
 }
 
-export default ({ data }) => (
-    <div className='wrapper'>
-      <ChosenClip clip={data.nodeClip} />
-      <h4>All Clips</h4>
-      <AllClips data={data}/>
-    </div>
-  )
+class ClipTemplate extends React.Component {
+  constructor(){
+    super()
+    this.state = {}
+  }
+  render() {
+    const { data } = this.props
+    return (
+      <div className='wrapper'>
+        <ChosenClip clip={data.nodeClip} />
+        <div style={{ padding: 10, margin: 10, border: `1px solid black`}}>
+          <button onClick={() => { this.setState({ teaching: false}) }}>
+            All Content
+          </button>
+          <button onClick={() => { this.setState({ teaching: true}) }}>
+            Teaching
+          </button>
+          {
+            (data.nodeClip.relationships.field_re || [])
+            .filter(node => (!this.state.teaching || node.field_include_in_the_teaching_se) )
+            .map((node, i) => {
+                if (node.__typename == `node__article`) {
+                  return (
+                    <ArticleCard
+                      i={i}
+                      article={node}
+                      relatedContent
+                    />
+                  )
+                } else if (node.__typename == `node__faq`) {
+                  return (
+                    <FAQCard
+                      i={i}
+                      faq={node}
+                    />
+                  )
+                } else if (node.__typename == `node__clip`) {
+                  return (
+                    <ClipCard
+                      i={i}
+                      clip={node}
+                      playable
+                    />
+                  )
+                }
+              }
+            )
+          }
+        </div>
+        <h4>All Clips</h4>
+        <AllClips data={data}/>
+      </div>
+    )
+  }
+}
+
+export default ClipTemplate;
 
 export const clipQuery = graphql`
   query clipQuery($id: String) {
