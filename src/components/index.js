1aa55c6f7eaeabb815d3c1db064ef09d65f6e964
Eugene Hauptmann <eugene.hp2012@gmail.com>
2018-08-10 05:27:14 -0700

    bump

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,9 @@ export Link from './Link'
 export Title from './Title'
 export ThemeCard from './ThemeCard'
 export SubthemeContainer from './SubthemeContainer'
-export AllClips from './AllClips'
+export AllClips from './allClips'
 export Card from './Card';
-export RCCard from './RCCard';
+export RCCard from './rccard';
 export Filter from './Filter';
 export { 
   Overlay, 
