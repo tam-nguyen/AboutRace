fbf3b7428c043bb1c305476cc4e27322c3c55463
Eugene Hauptmann <eugene.hp2012@gmail.com>
2018-08-26 07:16:49 -0700

    prep for the images

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,10 +2,41 @@ import React from 'react'
 import Link from '../Link'
 import styled from 'styled-components'
 
-const Container = styled.div`
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Row = styled.div`
   display: flex;
   flex-direction: row;
+`
+
+const MobileRow = styled.div`
+  width: 100vw;
+
+  display: flex;
+  flex-direction: row;
+
+  @media (min-width: 1025px) { /* desktop */
+    
+  }
+
+  @media (max-width: 812px) { /* mobile */
+    flex-direction: column;
+  }
+`
+
+const Container = styled(Column)`
   justify-content: center;
+  align-items: center;
+
+  padding-top: 64px;
+  padding-bottom: 64px;
+`
+
+const ImageHolder = styled(Column)`
+  flex: 1;
   align-items: center;
 `
 
@@ -13,7 +44,14 @@ class Footer extends React.Component {
   render() {
     return (
       <Container>
-        Footer
+        <Row>a production of:</Row>
+        <MobileRow>
+          <ImageHolder>1</ImageHolder>
+          <ImageHolder>2</ImageHolder>
+          <ImageHolder>3</ImageHolder>
+        </MobileRow>
+        <Row>website legaleze all content in this site reflects the views of its respective authors and in no way should be construed to represent the views of UC Berkeley or California Newsreel except where explicitly cited etc.
+Copyright 2018 UC Berkeley.</Row>
       </Container>
     )
   }
