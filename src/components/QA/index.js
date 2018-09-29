8d3082f0153680ef5a183f9fbc6f5b80528dfb75
tam-nguyen <tamquocnguyen@gmail.com>
2018-09-28 21:23:58 -0700

    navigating Q&A cards using the left/right arrows sometimes goes to 404

--- src/components/QA/index.js
+++ src/components/QA/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import kebabCase from 'lodash/kebabCase'
+import kebabCase from '../../utils/kebabCase'
 import get from 'lodash/get'
 
 import {
